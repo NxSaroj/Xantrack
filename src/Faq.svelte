f04de3e1389159d759f3a948a51b07aefc0b6606
Syst <153706805+NxSaroj@users.noreply.github.com>
2024-02-20 18:52:43 +0530

    Added FAQ

---
+++ src/Faq.svelte
@@ -0,0 +1,60 @@
+<ul class="max-w-2xl mx-auto mt-14 divide-y  shadow shadow-blue-600 rounded-xl">
+    <li>
+        <details class="group">
+            <summary class="flex items-center gap-3 px-4 py-3 font-medium marker:content-none hover:cursor-pointer">
+                <svg class="w-5 h-5 text-gray-500 transition group-open:rotate-90" xmlns="http://www.w3.org/2000/svg"
+                    width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z">
+                    </path>
+                </svg>
+                <span>What am I getting as a Premium Member?</span>
+            </summary>
+
+            <article class="px-4 pb-4">
+                <p class="underline font-bold">
+                    You will gain access to advanced features such as automod, music, notifications, and an increased triggers limit.
+                </p>
+            </article>
+        </details>
+    </li>
+    <li>
+        <details class="group">
+            <summary class="flex items-center gap-3 px-4 py-3 font-medium marker:content-none hover:cursor-pointer">
+                <svg class="w-5 h-5 text-gray-500 transition group-open:rotate-90" xmlns="http://www.w3.org/2000/svg"
+                    width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z">
+                    </path>
+                </svg>
+                <span>How can i get help Regarding Xantrack</span>
+            </summary>
+
+            <article class="px-4 pb-4">
+                <p class="underline font-bold">
+                    Feel free to join our support server using the navbar above. We are committed to doing our best to meet your satisfaction.    
+                </p>
+            </article>
+        </details>
+    </li>
+    <li>
+        <details class="group">
+            <summary class="flex items-center gap-3 px-4 py-3 font-medium marker:content-none hover:cursor-pointer">
+                <svg class="w-5 h-5 text-gray-500 transition group-open:rotate-90" xmlns="http://www.w3.org/2000/svg"
+                    width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z">
+                    </path>
+                </svg>
+                <span>Is The bot fully ready & Optimized Now ?</span>
+            </summary>
+
+            <article class="px-4 pb-4">
+                <p class="font-bold underline">
+                    Xantrack is currently under construction, with numerous commands set to be added soon. However, it is not yet ready for use.
+                </p>
+            </article>
+        </details>
+    </li>
+    
+</ul>
---
+++ src/FaqText.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<h1 class="faq text-red-600 text-3xl text-center mt-10 font-bold underline decoration-red-600 decoration-4">FAQ</h1>
+
+<style>
+     .faq {
+    font-family: "Poppins", sans-serif;
+  }
+</style>
